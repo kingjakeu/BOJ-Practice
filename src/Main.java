698c0994fb72dd869fc606aaec646ef5b651bfce
HyoSang Yoo <jakeyoo12@gmail.com>
2019-04-08 16:31:26 +0900

    #2178 미로찾기 해

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.LinkedList;
+
+/**
+ * BOJ #2178 미로탐색
+ * 분류: BFS
+ */ 
 
 public class Main {
-
+    public static void main(String[] args){
+    }
 }
+
+
+
