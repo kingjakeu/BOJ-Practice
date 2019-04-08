acb3ce4d04dd5606c9972793babe454bafaf1d71
HyoSang Yoo <jakeyoo12@gmail.com>
2019-04-08 17:53:18 +0900

    #2606 바이러스 푸는

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,69 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.Queue;
 import java.util.LinkedList;
-
-/**
- * BOJ #2178 미로탐색
- * 분류: BFS
- */ 
+import java.util.Queue;
+import java.util.StringTokenizer;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args){
+        CustomScanner cs = new CustomScanner();
+        int n = cs.nextInt();
+        int t = cs.nextInt();
+        int answer = 0;
+        int[] cache = new int[n];
+        int[][] map = new int[n][n];
+        for(int i=0; i<t; i++){
+            int from = cs.nextInt();
+            int to = cs.nextInt();
+            map[from-1][to-1] = 1;
+        }
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(0);
+
+        while(!que.isEmpty()){
+            int k = que.poll();
+            //print(cache);
+            for(int i=0; i<n; i++){
+                if(map[k][i] == 1){
+                    que.offer(i);
+                    cache[i] = 1;
+                }
+            }
+        }
+        for(int i=0; i<n; i++) {
+        	answer += cache[i];
+        }
+        System.out.println(answer);
+    }
+    static class CustomScanner{
+        static BufferedReader br;
+        static StringTokenizer st;
+
+        CustomScanner(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+            st = new StringTokenizer("");   
+        }
+        static String readLine(){
+            try{
+                return br.readLine();
+            }catch(IOException e){
+                return null;
+            }
+        }
+        static String next(){
+            while(!st.hasMoreTokens()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch(IOException e){
+
+                }
+            }
+            return st.nextToken();
+        }
+        static int nextInt(){
+            return Integer.parseInt(next());
+        }
     }
 }
-
-
 
