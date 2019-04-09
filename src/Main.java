365bd4db97b621e178c836f650c0f3bfcaf9ee4e
HyoSang Yoo <jakeyoo12@gmail.com>
2019-04-09 20:54:26 +0900

    #2468 안전구역 풀

--- src/Main.java
+++ src/Main.java
@@ -9,93 +9,84 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args){
-        CustomScanner cs = new CustomScanner();
-        int n = cs.nextInt();
-        int[][] map = new int[n][n];
-        int[][] off = {{0,1},{0,-1},{1,0},{-1,0}};
-        int count = 1;
-        for(int i=0; i<n; i++) {
-        	String t = cs.readLine();
-        	String[] ts = t.split("");
-        	for(int j=0; j<n; j++) {
-        		map[i][j] = Integer.parseInt(ts[j]);
-        	}
-        }
-        Queue<DOT> que = new LinkedList<>();
-        ArrayList<Integer> ans = new ArrayList<>();
-        
-        for(int i=0; i<n; i++) {
-        	for(int j=0; j<n; j++) {
-        		if(map[i][j] == 1) {
-        			que.add(new DOT(i,j));
-        			count++;
-        			map[i][j] = count;
-        			ans.add(0);
-        			while(!que.isEmpty()) {
-        				DOT d = que.poll();
-        				ans.set(count-2, ans.get(count-2)+1);
-        				
-        				for(int k=0; k<4; k++){
-        					int x = d.x;
-                        	int y = d.y;
-                        	x += off[k][1];
-                            y += off[k][0];
-                            if(y>=0 && y< n && x>=0 &&x<n){
-                                if(map[y][x] == 1){
-                                    map[y][x] = map[d.y][d.x];
-                                    DOT t = new DOT(y,x);
-                                    que.offer(t);
-                                }
-                            }
-        				}
-        			}
-        		}
-        	}
-        }
+    	CustomScanner sc = new CustomScanner();
+    	int n = sc.nextInt();
+    	int[][] map = new int[n][n];
+    	int max = 1;
+    	int maxH = 0;
+    	for(int i=0; i<n; i++) {
+    		for(int j=0; j<n; j++) {
+    			map[i][j] = sc.nextInt();
+    			maxH = Math.max(maxH, map[i][j]);
+    		}
+    	}
+    	
+    	for(int th=1; th<=maxH; th++) {
+    		int count = 1;
+    		int[][] cache = new int[n][n];
+    		Queue<DOT> que = new LinkedList<>();
+    		//인접하다 할수 있는 오프셋 상하좌우 
+    		int[][] off = {{1,0},{-1,0},{0,1},{0,-1}};
+    		
+    		
+    		for(int i=0; i<n; i++) {
+    			for(int j=0; j<n; j++) {
+    				// 방문한적없고 침수 높이보다 높으면 
+    				if(cache[i][j] == 0 && map[i][j]>th) {
+       	    			que.add(new DOT(i,j));
+       	    			cache[i][j] = count++;
+       	    			// 넣고 다음 카운트 증가
+       	    			
+        	    		while(!que.isEmpty()) {
+        	    			DOT d = que.poll();
+        	    			for(int k=0; k<4; k++) {
+        	    				int nx = d.x+off[k][0];
+        	    				int ny = d.y+off[k][1];
+        	    				if(nx>=0 && nx<n && ny>=0 && ny<n) {
+        	    					//인접한 곳으로 기기
+        	    					if(cache[ny][nx] == 0 && map[ny][nx] >th) {
+        	    						cache[ny][nx] = cache[d.y][d.x];
+        	    						que.add(new DOT(ny,nx));
+        	    					}
+        	    				}
+        	    			}
+        	    		}
+    				}
+    	    		
+    			}
+    		}
+    		max = Math.max(max, --count);
+    		
+    	}
 
-        System.out.println(count-1);
-        Collections.sort(ans);
-        for(int q : ans) {
-        	System.out.println(q);
-        }
-        
+		System.out.println(max);
     }
+    
     static class CustomScanner{
-        static BufferedReader br;
-        static StringTokenizer st;
-
-        CustomScanner(){
-            br = new BufferedReader(new InputStreamReader(System.in));
-            st = new StringTokenizer("");   
-        }
-        static String readLine(){
-            try{
-                return br.readLine();
-            }catch(IOException e){
-                return null;
-            }
-        }
-        static String next(){
-            while(!st.hasMoreTokens()){
-                try{
-                    st = new StringTokenizer(br.readLine());
-                }catch(IOException e){
-
-                }
-            }
-            return st.nextToken();
-        }
-        static int nextInt(){
-            return Integer.parseInt(next());
-        }
+    	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	static StringTokenizer st = new StringTokenizer("");
+    	
+    	static String next() {
+    		while(!st.hasMoreTokens()) {
+        		try {
+        			st = new StringTokenizer(br.readLine());
+        		}catch(IOException e) {
+        			
+        		}
+    		}
+    		return st.nextToken();
+    	}
+    	static int nextInt() {
+    		return Integer.parseInt(next());
+    	}
     }
 }
 class DOT{
-	int x; 
+	int x;
 	int y;
 	DOT(int _y, int _x){
 		y = _y;
 		x = _x;
 	}
+	
 }
-
