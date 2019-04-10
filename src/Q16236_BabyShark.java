2000337d347df9dde5b8a4878e65fd592c20970b
kingjake <jakeyoo12@gmail.com>
2019-04-11 01:49:55 +0900

    #16236 아기상어 해결

--- src/Q16236_BabyShark.java
+++ src/Q16236_BabyShark.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-import java.io.IOException;
 
-public class Main {
-	static int[][] map;
+import Main.CustomScanner;
+
+public class Q16236_BabyShark {
+static int[][] map;
 	
 	public static void main(String[] args) {
 		CustomScanner sc = new CustomScanner();
