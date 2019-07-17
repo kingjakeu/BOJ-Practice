e9b1ab49cd920697bf0a136e517007c968a61054
HyoSang Yoo <jakeyoo12@gmail.com>
2019-07-17 18:01:10 +0900

    Solve LeetCode #28

---
+++ src/LEETCODE/Q28_ImplementstrStr.java
@@ -0,0 +1,10 @@
+package LEETCODE;
+
+public class Q28_ImplementstrStr {
+}
+
+class Q28_ImplementstrStr_Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
