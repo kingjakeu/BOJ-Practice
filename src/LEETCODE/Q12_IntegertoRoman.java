ee1fc3ea566a45622a4c43a8c6ab62ba4187cd6c
kingjake <jakeyoo12@gmail.com>
2019-07-29 22:44:21 +0900

    Solve leetcode #12

---
+++ src/LEETCODE/Q12_IntegertoRoman.java
@@ -0,0 +1,37 @@
+package LEETCODE;
+
+public class Q12_IntegertoRoman {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.intToRoman(58));
+    }
+}
+class Solution {
+    public String intToRoman(int num) {
+        String[] symbol = {"I", "V", "X", "L", "C", "D", "M"};
+        int[] value = {1, 5, 10, 50, 100, 500, 1000};
+        StringBuilder sb = new StringBuilder("");
+        for(int i = 6; i >= 0; i = i-2){
+            int divd = num/value[i];
+            num = num%value[i];
+            if(divd==9){
+                sb.append(symbol[i]);
+                sb.append(symbol[i+2]);
+            }else if(divd==4){
+                sb.append(symbol[i]);
+                sb.append(symbol[i+1]);
+            }else{
+                if(divd>=5){
+                    sb.append(symbol[i+1]);
+                    divd = divd-5;
+                    num = num%value[i+1];
+                }
+                for (int j = 0; j < divd; j++) {
+                    sb.append(symbol[i]);
+                }
+            }
+
+        }
+        return sb.toString();
+    }
+}
