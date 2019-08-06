309467b9be8ca8e51c40f1ce277e0eedf8a15f13
HyoSang Yoo <jakeyoo12@gmail.com>
2019-08-06 09:24:03 +0900

    Solve LeetCode #48

---
+++ src/LEETCODE/Q48_RotateImage.java
@@ -0,0 +1,20 @@
+package LEETCODE;
+
+public class Q48_RotateImage  {
+}
+class Solution {
+    public void rotate(int[][] matrix) {
+        int leng = matrix.length;
+        int[][] answer = new int[leng][leng];
+        for (int i = 0; i < leng; i++) {
+            for (int j=0; j<leng; j++){
+                answer[j][leng-i-1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < leng; i++) {
+            for (int j=0; j<leng; j++){
+               matrix[i][j] = answer[i][j];
+            }
+        }
+    }
+}
