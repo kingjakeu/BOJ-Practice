cba2f5f4b6f282e5dedc4942fb99737128cb9674
HyoSang Yoo <jakeyoo12@gmail.com>
2019-08-29 17:36:44 +0900

    Solving LeetCode #57

---
+++ src/LEETCODE/Q57_InsertInterval.java
@@ -0,0 +1,48 @@
+package LEETCODE;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q57_InsertInterval {
+}
+class Q57_InsertInterval_Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int[][] answer;
+        List<int[]> tempAnswer = new LinkedList<>();
+
+        int min = newInterval[0];
+        int max = newInterval[1];
+
+        for(int[] intvl : intervals){
+            if((intvl[0]<=min || intvl[0]<=max)&&(intvl[1]>=min || intvl[1]>=max)){
+                min = Math.min(intvl[0], min);
+                max = Math.max(intvl[1], max);
+            }else{
+                tempAnswer.add(intvl);
+            }
+        }
+        int[] ans = {min,max};
+        tempAnswer.add(ans);
+
+        int size = tempAnswer.size();
+        answer = new int[size][2];
+        tempAnswer.sort(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0]<o2[0]){
+                    return -1;
+                }else if(o1[0]>o2[0]){
+                    return 1;
+                }
+                return 0;
+            }
+        });
+        int i = 0;
+        for(int[] tmp : tempAnswer){
+            answer[i] = tmp;
+            i++;
+        }
+        return answer;
+    }
+}
