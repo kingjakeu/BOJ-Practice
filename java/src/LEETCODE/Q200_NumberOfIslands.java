141907f1304967c679ca3864995b8d8121efcdb7
kingjake <jakeyoo12@gmail.com>
2019-12-18 22:48:29 +0900

    solved Q200 leetcode

--- java/src/LEETCODE/Q200_NumberOfIslands.java
+++ java/src/LEETCODE/Q200_NumberOfIslands.java
@@ -5,63 +5,40 @@ public class Q200_NumberOfIslands {
     public static void main(String[] args) {
         char[][] grid = {{'1','1','1'},{'0','1','0'},{'1','1','1'}};
         Q200_Solution s = new Q200_Solution();
-        s.numIslands(grid);
+        System.out.println(s.numIslands(grid));
     }
 
 
 }
 class Q200_Solution {
+    static int h;
+    static int w;
     public int numIslands(char[][] grid) {
-        int h = grid.length;
-        if(h == 0){
-            return 0;
-        }
-        int w = grid[0].length;
-        int[][] unionMap = new int[h][w];
+        h = grid.length;
+        if(h == 0) return 0;
+        w = grid[0].length;
+
+        int count = 0;
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
-                unionMap[i][j] = 0;
-                for (int k = 0; k < h; k++) {
-                    for (int l = 0; l < w; l++) {
-                        System.out.print(unionMap[k][l]+' ');
-                    }
-                    System.out.println();
-                }
-                System.out.println();
+               if(grid[i][j] == '1'){
+                   finding(grid, i, j);
+                   count += 1;
+               }
             }
         }
 
-        int[][] offset = {{0,-1}, {-1,0}};
-        int count = 1;
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                if(grid[i][j] == '1'){
-                    for (int k = 0; k < 2; k++) {
-                        int dy = i+offset[k][0];
-                        int dx = j+offset[k][1];
-                        if(dy>=0 && dx >=0){
-                            if(grid[dy][dx] == '1'){
-                                unionMap[i][j] = unionMap[dy][dx];
-                                break;
-                            }
-                        }
-                    }
-                    if(unionMap[i][j] == 0){
-                        unionMap[i][j] = count;
-                        System.out.println(i+" "+j+" "+count);
-                        count += 1;
-                    }
-                    for (int k = 0; k < h; k++) {
-                        for (int l = 0; l < w; l++) {
-                            System.out.print(unionMap[k][l]+' ');
-                        }
-                        System.out.println();
-                    }
-                    System.out.println();
-                }
-
-            }
+        return count;
+    }
+    public void finding(char[][] grid, int y, int x){
+        if(y < 0 || x < 0 || y > h-1 || x > w-1 || grid[y][x] != '1'){
+            return;
+        }else{
+            grid[y][x] = '0';
+            finding(grid, y-1, x);
+            finding(grid, y+1, x);
+            finding(grid, y, x-1);
+            finding(grid, y, x+1);
         }
-        return count-1;
     }
 }
