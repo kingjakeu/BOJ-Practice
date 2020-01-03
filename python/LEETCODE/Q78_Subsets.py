440dbac15ac115374cad69d5906a08f6f2b7c95a
kingjake <jakeyoo12@gmail.com>
2020-01-03 23:34:38 +0900

    solve leetcode q87

---
+++ python/LEETCODE/Q78_Subsets.py
@@ -0,0 +1,9 @@
+from _ast import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for num in sorted(nums):
+            res += [item + [num] for item in res]
+        return res
