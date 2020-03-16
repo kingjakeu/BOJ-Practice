8d4e0a69cd3fb93c0079b2c8f40ef9758d55864f
kingjake <jakeyoo12@gmail.com>
2020-03-16 22:53:25 +0900

    Solve baekjoon sort2 in quicksort

---
+++ java/src/BAEKJOON/sort/Q2751_Sort2.java
@@ -0,0 +1,62 @@
+package BAEKJOON.sort;
+
+import java.io.*;
+
+public class Q2751_Sort2 {
+
+    static int n;
+    static int[] array;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(br.readLine());
+        }
+
+        sort(0, n-1);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int a : array){
+            bw.write(String.valueOf(a));
+            bw.newLine();
+        }
+        bw.close();
+    }
+
+    public static void sort(int low, int high){
+        if(low < high){
+            int m = (high+low)/2;
+            swap(low, m);
+            int j = partition(low, high);
+            sort(low, j-1);
+            sort(j+1, high);
+        }
+    }
+
+    public static int partition(int low, int high){
+        int i = low;
+        int j = high+1;
+
+        while (true){
+            while(array[++i] < array[low]){
+                if(i == high) break;
+            }
+            while (array[low] < array[--j]){
+                if (j == low) break;
+            }
+            if(i >= j) break;
+            swap(i, j);
+        }
+
+        swap(low, j);
+        return j;
+    }
+
+    public static void swap(int i, int j){
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+
+}
